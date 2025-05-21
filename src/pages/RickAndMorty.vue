816546fe567995c1ef13ae9db3f70d95a5ddca59
KSohkin <Kaupo.Sohkin@tptlive.ee>
2025-05-21 12:12:12 +0300

    Add infinite scroll

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -1,7 +1,7 @@
 <script setup>
 
 import axios from 'axios';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import CharacterCard from '../components/CharacterCard.vue';
 import SimplePagination from '../components/SimplePagination.vue';
 import Pagination from '../components/Pagination.vue';
@@ -26,12 +26,11 @@ async function getCharacters() {
             }
         });
         console.log(response.data);
-        characters.value = response.data.results;
+        characters.value.push(...response.data.results);
         info.value = response.data.info;
     } catch(err) {
         console.log(err);
         error.value = 'No results found';
-        characters.value = [];
         info.value = null;
     }
 }
@@ -60,6 +59,15 @@ async function search(){
     }, 1000);
 }
 
+onMounted(() => {
+    document.addEventListener('scroll', () => {
+        if(window.scrollY + window.innerHeight > document.body.clientHeight - 300 ) {
+            next();
+        }
+    })
+    
+});
+
 </script>
 <template>
     <div class="field has-addons">
@@ -73,7 +81,6 @@ async function search(){
         </div>
     </div>
 
-    <Pagination v-if="info" :info="info" :current="currentPage" @next="next" @prev="prev" @page="page"></Pagination>
     <div class="columns is-multiline">
         <div v-for="character in characters" class="column is-one-quarter">
             <CharacterCard :character="character"></CharacterCard>
